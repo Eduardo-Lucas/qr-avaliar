55f709c1a2f24b4bda53574caf297424d934d790
Eduardo Lucas <eduardolucas40@gmail.com>
2019-09-29 18:32:24 -0300

    Push to bitbucket

--- mysite/settings.py
+++ mysite/settings.py
@@ -92,17 +92,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# if DEBUG:  # DEBUG = True ==> Development
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:  # DEBUG = False ==> Production
-DATABASES = {
-    'default': dj_database_url.config()
-}
+if DEBUG:  # DEBUG = True ==> Development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:  # DEBUG = False ==> Production
+    DATABASES = {
+        'default': dj_database_url.config()
+    }
 
 
 # Password validation
